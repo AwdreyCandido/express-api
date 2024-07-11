e7aa8303feec2c766d7e4ec6f98a4d458e6c4282
AwdreyCandido <awdrey.dev@gmail.com>
2024-07-11 20:11:54 -0300

    feat: implementing crud operations

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -1,29 +1,64 @@
 import { Request, Response } from "express";
+import Database from "../services/database";
+import IProduct from "../interfaces/IProduct";
 
 export const getAllProducts = async (req: Request, res: Response) => {
-  res.json({ message: "Product Router - GET Method" });
+  const db = await Database.getIstance();
+  const [products] = await db.execute("SELECT * FROM products");
+
+  res
+    .status(200)
+    .json({ message: "Product Router - GET Method", data: products });
 };
 
 export const getProduct = async (req: Request, res: Response) => {
+  const db = await Database.getIstance();
   const id = req.params.id;
-  res.json({ message: `Product Router - GET Method - Query Id = ${id}` });
+  const [product] = await db.execute(
+    `SELECT * FROM products WHERE products.id = ${id}`
+  );
+  res.json({
+    message: `Product Router - GET Method - Query Id = ${id}`,
+    data: product,
+  });
 };
 
 export const addNewProduct = async (req: Request, res: Response) => {
-  const newProduct = req.body;
-  res.json({ message: `Product Router - POST Method`, data: newProduct });
+  const db = await Database.getIstance();
+  const newProduct: IProduct = req.body;
+
+  const [result] = await db.execute(
+    `INSERT INTO products (name, description, price) VALUES ("${newProduct.name}", "${newProduct.description}", ${newProduct.price})`
+  );
+
+  res.json({ message: `Product Router - POST Method`, data: result });
 };
 
 export const updateProduct = async (req: Request, res: Response) => {
+  const db = await Database.getIstance();
   const id = req.params.id;
-  const updatedProduct = req.body;
+  const updatedProduct: IProduct = req.body;
+
+  const [result] = await db.execute(`
+    UPDATE products SET name="${updatedProduct.name}", description="${updatedProduct.description}", price="${updatedProduct.price}" WHERE id = ${id}
+  `);
+
   res.json({
     message: `Product Router - PATCH Method - Query Id = ${id}`,
-    data: updatedProduct,
+    data: result,
   });
 };
 
 export const deleteProduct = async (req: Request, res: Response) => {
+  const db = await Database.getIstance();
   const id = req.params.id;
-  res.json({ message: `Product Router - DELETE Method - Query Id = ${id}` });
+
+  const [result] = await db.execute(`
+    DELETE FROM products WHERE id = "${id}"
+  `);
+
+  res.json({
+    message: `Product Router - DELETE Method - Query Id = ${id}`,
+    data: result,
+  });
 };
