8415048df9a62e3b26712c65b6071d5e251f45bf
AwdreyCandido <awdrey.dev@gmail.com>
2024-07-11 16:51:41 -0300

    refactor: improving database access singleton

--- src/services/database.ts
+++ src/services/database.ts
@@ -3,9 +3,11 @@ import mysql from "mysql2/promise";
 class Database {
   private static instance: mysql.Connection | null = null;
 
+  private constructor() {}
+
   public static async getIstance(): Promise<mysql.Connection> {
     if (this.instance == null) {
-      const instance: mysql.Connection = await mysql.createConnection({
+      this.instance = await mysql.createConnection({
         host: "localhost",
         user: "root",
         port: 3306,
@@ -13,7 +15,10 @@ class Database {
         database: "products",
       });
 
-      return instance;
+      this.instance.on("error", (error) => {
+        console.error("MySql connection error", error);
+        this.instance = null;
+      });
     }
 
     return this.instance;
