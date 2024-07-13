a7899d910786bace9b937711dee84d7ad3c7b8eb
AwdreyCandido <awdrey.dev@gmail.com>
2024-07-13 15:57:33 -0300

    feat: enabling https server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import productRouter from "./router/productRouter";
 import Database from "./services/database";
+import fs from "fs";
+import https from "https";
+import path from "path";
 
 const app = express();
 const database = Database.getInstance();
@@ -9,7 +12,16 @@ const PORT = 3000;
 app.use(express.json());
 app.use("/api/v1/products", productRouter);
 
-app.listen(PORT, async () => {
+// read SSL certificates and key files
+const options = {
+  key: fs.readFileSync(path.join(__dirname, "localhost-key.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "localhost.pem")),
+};
+
+// create HTTPS server
+const server = https.createServer(options, app);
+
+server.listen(PORT, async () => {
   try {
     await database.getConnection();
     console.log(`Server is running on https://localhost:${PORT}`);
