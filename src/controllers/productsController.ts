4a04812dd8ed97ab0932fb27148514e7281eb894
Awdrey Candido <awdrey.dev@gmail.com>
2024-07-17 00:11:00 -0300

    refactor: handling errors in products controller

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -7,48 +7,109 @@ const database = Database.getInstance();
 const productsRepository = new ProductsRepository(database);
 
 export const getAllProducts = async (req: Request, res: Response) => {
-  const products = await productsRepository.getAll();
+  try {
+    const products = await productsRepository.getAll();
 
-  res
-    .status(200)
-    .json({ message: "Product Router - GET Method", data: products });
+    res
+      .status(200)
+      .json({ message: "success", data: products });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: "Um erro ocorreu ao buscar os produtos.",
+        data: error
+      })
+  }
 };
 
 export const getProduct = async (req: Request, res: Response) => {
-  const id: number = +req.params.id;
-  const product = await productsRepository.find(id);
+  try {
+    const id: number = +req.params.id;
+    const product = await productsRepository.find(id);
 
-  res.json({
-    message: `Product Router - GET Method - Query Id = ${id}`,
-    data: product,
-  });
+    if (!product) {
+      return res.status(404).json({ message: "Produto não encontrado." })
+    }
+
+    res
+      .status(200)
+      .json({
+        message: "success",
+        data: product,
+      });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: "Um erro ocorreu ao buscar o produto.",
+        data: error
+      })
+  }
 };
 
 export const addNewProduct = async (req: Request, res: Response) => {
-  const newProduct: IProduct = req.body;
-  const result = await productsRepository.create(newProduct);
+  try {
+    const newProduct: IProduct = req.body;
+    const result = await productsRepository.create(newProduct);
 
-  res.json({ message: `Product Router - POST Method`, data: result });
+    res
+      .status(201)
+      .json({ message: "success", data: result });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: "Um erro ocorreu ao criar novo produto.",
+        data: error
+      })
+  }
 };
 
 export const updateProduct = async (req: Request, res: Response) => {
-  const id: number = +req.params.id;
-  const updatedProduct: IProduct = req.body;
+  try {
+    const id: number = +req.params.id;
+    const updatedProduct: IProduct = req.body;
 
-  const result = await productsRepository.update(id, updatedProduct);
+    const result = await productsRepository.update(id, updatedProduct);
 
-  res.json({
-    message: `Product Router - PATCH Method - Query Id = ${id}`,
-    data: result,
-  });
+    if (!result) {
+      return res.status(404).json({
+        message: "Produto não encontrado."
+      });
+    }
+
+    res
+      .status(200)
+      .json({
+        message: "success",
+        data: result,
+      });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: "Um erro ocorreu ao atualizar o produto.",
+        data: error
+      })
+  }
 };
 
 export const deleteProduct = async (req: Request, res: Response) => {
-  const id: number = +req.params.id;
-  const result = await productsRepository.delete(id);
+  try {
+    const id: number = +req.params.id;
+    const result = await productsRepository.delete(id);
 
-  res.json({
-    message: `Product Router - DELETE Method - Query Id = ${id}`,
-    data: result,
-  });
+    res.json({
+      message: "success",
+      data: result,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: "Um erro ocorreu ao excluir o produto.",
+        data: error
+      })
+  }
 };
